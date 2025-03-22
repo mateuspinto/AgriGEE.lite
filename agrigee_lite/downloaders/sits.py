69131a9bb5491dbcac83df19d3d372a903a4e8a3
Mateus Pinto da Silva <mateusps10@outlook.com>
2025-03-22 18:09:40 -0300

    refactor: rename download_single to download_single_sits and update date handling

--- agrigee_lite/downloaders/sits.py
+++ agrigee_lite/downloaders/sits.py
@@ -5,12 +5,15 @@ from shapely import Polygon
 from agrigee_lite.satellites.abstract_satellite import AbstractSatellite
 
 
-def download_single(
-    geometry: Polygon, start_date: pd.Timestamp, end_date: pd.Timestamp, satellite: AbstractSatellite
+def download_single_sits(
+    geometry: Polygon, start_date: pd.Timestamp | str, end_date: pd.Timestamp | str, satellite: AbstractSatellite
 ) -> pd.DataFrame:
+    start_date = start_date.strftime("%Y-%m-%d") if isinstance(start_date, pd.Timestamp) else start_date
+    end_date = end_date.strftime("%Y-%m-%d") if isinstance(end_date, pd.Timestamp) else end_date
+
     ee_feature = ee.Feature(
         ee.Geometry(geometry.__geo_interface__),
-        {"start_date": start_date.strftime("%Y-%m-%d"), "end_date": end_date.strftime("%Y-%m-%d"), "index_num": 1},
+        {"start_date": start_date, "end_date": end_date, "index_num": 1},
     )
     ee_expression = satellite.compute(ee_feature)
     return ee.data.computeFeatures({"expression": ee_expression, "fileFormat": "PANDAS_DATAFRAME"}).drop(
