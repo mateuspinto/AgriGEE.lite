dcc369fc7285e024f03da26fe9d37de9e2015b48
Mateus Pinto da Silva <mateusps10@outlook.com>
2025-03-24 15:49:03 -0300

    feat: add multithreaded download_multiple_sits function for improved performance

--- agrigee_lite/downloaders/sits.py
+++ agrigee_lite/downloaders/sits.py
@@ -1,9 +1,11 @@
+import concurrent.futures
 from functools import partial
 
 import ee
 import geopandas as gpd
 import pandas as pd
 from shapely import Polygon
+from tqdm.std import tqdm
 
 from agrigee_lite.ee_utils import ee_gdf_to_feature_collection
 from agrigee_lite.satellites.abstract_satellite import AbstractSatellite
@@ -31,3 +33,64 @@ def download_multiple_sits(gdf: gpd.GeoDataFrame, satellite: AbstractSatellite) 
     return ee.data.computeFeatures({"expression": ee_expression, "fileFormat": "PANDAS_DATAFRAME"}).drop(
         columns=["geo"]
     )
+
+
+def download_multiple_sits_multithread(
+    gdf: pd.DataFrame,
+    satellite: AbstractSatellite,
+    chunksize: int = 10,
+    num_threads_rush: int = 30,
+    num_threads_retry: int = 10,
+) -> pd.DataFrame:
+    indexes_with_errors: list[int] = []
+    whole_result_df = pd.DataFrame()
+    num_chunks = (len(gdf) + chunksize - 1) // chunksize
+    all_chunk_ids = list(range(num_chunks))
+
+    def process_download(gdf_chunk: gpd.GeoDataFrame, i: int) -> pd.DataFrame:
+        try:
+            result_chunk = download_multiple_sits(gdf_chunk, satellite)
+            result_chunk["chunk_id"] = i
+            return result_chunk  # noqa: TRY300
+        except Exception as e:
+            print(str(e))
+            return pd.DataFrame()
+
+    def run_downloads(chunk_ids: list[int], num_threads: int, desc: str) -> None:
+        nonlocal whole_result_df
+        error_count = 0
+
+        with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
+            futures = [
+                executor.submit(
+                    partial(process_download, gdf.iloc[i * chunksize : (i + 1) * chunksize].reset_index(drop=True), i)
+                )
+                for i in chunk_ids
+            ]
+
+            pbar = tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc=desc)
+            for future in pbar:
+                result_df_chunk = future.result()
+                if result_df_chunk.empty:
+                    error_count += 1
+                    indexes_with_errors.extend(result_df_chunk.index)
+                    pbar.set_postfix({"errors": error_count})
+                else:
+                    whole_result_df = pd.concat([whole_result_df, result_df_chunk])
+
+    run_downloads(all_chunk_ids, num_threads=num_threads_rush, desc="Downloading")
+
+    if indexes_with_errors:
+        run_downloads(
+            indexes_with_errors,
+            num_threads=num_threads_retry,
+            desc="Re-running failed downloads",
+        )
+
+    whole_result_df["index_num"] = (
+        whole_result_df["chunk_id"] * (whole_result_df["index_num"].max() + 1) + whole_result_df["index_num"]
+    )
+    whole_result_df.drop(columns=["chunk_id"], inplace=True)
+    whole_result_df = whole_result_df.sort_values("index_num", kind="stable").reset_index(drop=True)
+
+    return whole_result_df
