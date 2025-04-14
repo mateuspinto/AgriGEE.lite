84013f885758f0f2d7bf25bb5db94e3df34161d7
Mateus Pinto da Silva <mateusps10@outlook.com>
2025-04-14 17:50:03 -0300

    fix: add sits chunk in init

--- agrigee_lite/get/__init__.py
+++ agrigee_lite/get/__init__.py
@@ -6,5 +6,7 @@ from agrigee_lite.get.sits import (
     multiple_sits_multithread,
     multiple_sits_task_gcs,
     multiple_sits_task_gdrive,
+    multiple_sits_chunks_gdrive,
+    multiple_sits_chunks_gcs,
 )
 from agrigee_lite.get.sits import single_sits as sits
