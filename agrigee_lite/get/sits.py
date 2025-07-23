fix: improve handling of empty image collections and DataFrames; update maxPixels calculation in MapBiomas; add UnifiedSatellite class

agrigee_lite/get/sits.py
@@ -234,7 +234,6 @@ async def __download_multiple_sits_async(
                                 subsampling_max_pixels=subsampling_max_pixels,
                             )
                         )
-                        chunk_result_df["chunk_id"] = chunk_id
 
                     whole_result_df = pd.concat([whole_result_df, chunk_result_df])
                 except KeyboardInterrupt:
@@ -316,7 +315,10 @@ def download_multiple_sits_chunks_multithread(
                 num_threads_retry=retry_concurrency,
                 pbar=pbar,
             )
-            chunk_df["timestamp"] = pd.to_datetime(chunk_df.timestamp)
+
+            if len(chunk_df) != 0:
+                chunk_df["timestamp"] = pd.to_datetime(chunk_df.timestamp)
+
             for col in chunk_df.columns:
                 if col not in ["timestamp", "indexnum"]:
                     chunk_df[col] = chunk_df[col].astype(np.float16)
@@ -328,7 +330,8 @@ def download_multiple_sits_chunks_multithread(
         df = pd.read_parquet(f)
         whole_result_df = pd.concat([whole_result_df, df], ignore_index=True)
 
-    whole_result_df = whole_result_df.sort_values(by=["indexnum"], kind="stable").reset_index(drop=True)
+    if len(whole_result_df) != 0:
+        whole_result_df = whole_result_df.sort_values(by=["indexnum"], kind="stable").reset_index(drop=True)
 
     return whole_result_df
 

agrigee_lite/vis/images.py
@@ -1,3 +1,5 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,24 +23,35 @@ def visualize_multiple_images(
         geometry, start_date, end_date, satellite, invalid_images_threshold, num_threads_rush, num_threads_retry
     )
 
+    if len(images) == 0:
+        print("No images found for the specified parameters.")
+        return
+
     images = np.clip(images * contrast, 0, 1)
 
     images_per_row = 10
     num_rows = (len(image_names) // images_per_row) + (len(image_names) % images_per_row > 0)
 
     fig, ax = plt.subplots(
-        num_rows, images_per_row, figsize=(25, (len(image_names) // images_per_row) * 5), sharex=True, sharey=True
+        num_rows,
+        images_per_row,
+        figsize=(25, (math.ceil(len(image_names) / images_per_row)) * 5),
+        sharex=True,
+        sharey=True,
     )
+
+    ax = ax.flatten()
+
     for i in range(num_rows * images_per_row):
         if i < len(image_names):
             name = image_names[i]
             image = images[i]
 
-            ax[i // images_per_row, i % images_per_row].imshow(image)
-            ax[i // images_per_row, i % images_per_row].set_title(name)
-            ax[i // images_per_row, i % images_per_row].axis("off")
+            ax[i].imshow(image)
+            ax[i].set_title(name)
+            ax[i].axis("off")
         else:
-            fig.delaxes(ax[i // images_per_row, i % images_per_row])
+            fig.delaxes(ax[i])
 
     plt.tight_layout()
     plt.show()